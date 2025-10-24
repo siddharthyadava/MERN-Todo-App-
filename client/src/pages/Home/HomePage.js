ed1db2eff374f97e83ac8e974dc6828eb6278b6b
siddharthyadava <siddharthyadav2904@gmail.com>
2025-10-24 23:07:36 +0530

    added Navbar and error messages

--- client/src/pages/Home/HomePage.js
+++ client/src/pages/Home/HomePage.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../../components/Layout/Navbar'
 
 const HomePage = () => {
   return (
     <div>
+      <Navbar />
       <h1>Home Page</h1>
     </div>
   )
