98aff9df5b233e35ed3728822cb9261e80c8ca3d
siddharthyadava <siddharthyadav2904@gmail.com>
2025-12-15 17:04:27 +0530

    Added forgot Password Functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const userModel = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const JWT = require("jsonwebtoken");
+const crypto = require("crypto");
 
 // REGISTER
 const registerController = async (req, res) => {
@@ -90,4 +91,100 @@ const loginControler = async (req, res) => {
   }
 };
 
-module.exports = { registerController, loginControler };
+//FORGOT PASSWORD
+const forgotPasswordController = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    if (!email) {
+      return res.status(400).send({
+        success: false,
+        message: "Please provide email",
+      });
+    }
+
+    const user = await userModel.findOne({ email });
+
+    // Do not reveal if user exists or not
+    if (!user) {
+      return res.status(200).send({
+        success: true,
+        message: "If this email is registered, a password reset link has been sent",
+      });
+    }
+
+    // Create token
+    const resetToken = crypto.randomBytes(32).toString("hex");
+
+    user.resetPasswordToken = resetToken;
+    user.resetPasswordExpire = Date.now() + 60 * 60 * 1000; // 1 hour
+    await user.save();
+
+    const resetUrl = `${process.env.CLIENT_URL}/reset-password/${resetToken}`;
+    
+    // TODO: send email here using nodemailer or any email service
+    // for now, log it so you can test:
+    console.log("Password reset link:", resetUrl);
+
+    return res.status(200).send({
+      success: true,
+      message: "If this email is registered, a password reset link has been sent",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in forgot password API",
+    });
+  }
+};
+
+// RESET PASSWORD
+const resetPasswordController = async (req, res) => {
+  try {
+    const { token } = req.params;
+    const { password } = req.body;
+
+    if (!password) {
+      return res.status(400).send({
+        success: false,
+        message: "Please provide new password",
+      });
+    }
+
+    const user = await userModel.findOne({
+      resetPasswordToken: token,
+      resetPasswordExpire: { $gt: Date.now() }, // token not expired
+    });
+
+    if (!user) {
+      return res.status(400).send({
+        success: false,
+        message: "Invalid or expired password reset token",
+      });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    user.password = hashedPassword;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+
+    return res.status(200).send({
+      success: true,
+      message: "Password reset successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in reset password API",
+      error,
+    });
+  }
+};
+
+module.exports = { registerController, loginControler, forgotPasswordController, resetPasswordController };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,6 +2,8 @@ const express = require("express");
 const {
   registerController,
   loginControler,
+  forgotPasswordController,
+  resetPasswordController,
 } = require("../controllers/userController");
 
 //router object
@@ -13,6 +15,12 @@ router.post("/register", registerController);
 
 //LOGIN || POST
 router.post("/login", loginControler);
+
+// FORGOT PASSWORD // POST
+router.post("/forgot-password", forgotPasswordController);
+
+// RESET PASSWORD // POST
+router.post("/reset-password/:token", resetPasswordController)
 
 //export
 module.exports = router;
